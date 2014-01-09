17a620ecdf4b0d5fc546d322510e0c4ddfc0c16e
Celeo <celeodor@gmail.com>
2014-01-09 12:03:15 -0800

    Recent scan edits page - removing duplicates

--- views.py
+++ views.py
@@ -580,13 +580,15 @@ def recent_scan_edits(request):
     wormholes = []
     count = 0
     for s in SiteChange.objects.filter(changedScanid=True).order_by('-id'):
-        sites.append(s.site)
+        if not site in sites:
+            sites.append(s.site)
         count += 1
         if count == int(appSettings.RECENT_EDITS_LIMIT) + 1:
             break
     count = 0
     for w in WormholeChange.objects.filter(changedScanid=True).order_by('-id'):
-        wormholes.append(w.wormhole)
+        if not wormhole in wormholes:
+            wormholes.append(w.wormhole)
         count += 1
         if count == int(appSettings.RECENT_EDITS_LIMIT) + 1:
             break
